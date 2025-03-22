ee22c9db0179d3c04c2f6d4a9d68c04166f598ee
Adrenalin <159599669+akuadre@users.noreply.github.com>
2025-03-22 21:00:10 +0700

    Delete resources directory

--- resources/views/admin/v_footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0.0
-    </div>
-    <strong>Copyright &copy; {{ date('Y') }} <a href="https://agussuratna.net" target="blank">The Letter</a>.</strong> All rights reserved.
-</footer>
