20e2daf3bb218e2cdf9caafd75af798dbb3d5b1e
Adre <dreenation21@gmail.com>
2025-06-07 20:10:46 +0700

    feat: update route web.php

--- routes/web.php
+++ routes/web.php
@@ -45,57 +45,39 @@ Route::get('/home', [HomeController::class,'home'])->name('home')->middleware('a
 Route::get('/about', [HomeController::class,'about'])->middleware('auth');
 //=========================AKHIR ROUTE HOME=========================
 
+
 //=========================AWAL ROUTE PROGRAM KEAHLIAN=========================
-Route::get('/programkeahlian', [ProgramKeahlianController::class,'programkeahlian'])->middleware('auth');
-Route::post('/programkeahlian/tambah',[ProgramKeahlianController::class,'programkeahliantambah'])->middleware('auth');
-Route::get('/programkeahlian/hapus/{idprogramkeahlian}',[ProgramKeahlianController::class,'programkeahlianhapus'])->middleware('auth');
-Route::put('/programkeahlian/edit/{idprogramkeahlian}', [ProgramKeahlianController::class,'programkeahlianedit'])->middleware('auth');
+// Route::get('/programkeahlian', [ProgramKeahlianController::class,'programkeahlian'])->middleware('auth');
+// Route::post('/programkeahlian/tambah',[ProgramKeahlianController::class,'programkeahliantambah'])->middleware('auth');
+// Route::get('/programkeahlian/hapus/{idprogramkeahlian}',[ProgramKeahlianController::class,'programkeahlianhapus'])->middleware('auth');
+// Route::put('/programkeahlian/edit/{idprogramkeahlian}', [ProgramKeahlianController::class,'programkeahlianedit'])->middleware('auth');
 //========================AKHIR ROUTE PROGRAM KEAHLIAN========================
 
 
 
 
 //=========================AWAL ROUTE JURUSAN=========================
-Route::get('/jurusan', [JurusanController::class,'jurusan'])->middleware('auth');
-Route::post('/jurusan/tambah',[JurusanController::class,'jurusantambah'])->middleware('auth');
-Route::get('/jurusan/hapus/{idjurusan}',[JurusanController::class,'jurusanhapus'])->middleware('auth');
-Route::put('/jurusan/edit/{idjurusan}', [JurusanController::class,'jurusanedit'])->middleware('auth');
+// Route::get('/jurusan', [JurusanController::class,'jurusan'])->middleware('auth');
+// Route::post('/jurusan/tambah',[JurusanController::class,'jurusantambah'])->middleware('auth');
+// Route::get('/jurusan/hapus/{idjurusan}',[JurusanController::class,'jurusanhapus'])->middleware('auth');
+// Route::put('/jurusan/edit/{idjurusan}', [JurusanController::class,'jurusanedit'])->middleware('auth');
 //========================AKHIR ROUTE JURUSAN========================
 
 
 
 //=========================AWAL ROUTE KELAS=========================
-Route::get('/kelas', [KelasController::class,'kelas'])->middleware('auth');
-Route::post('/kelas/tambah',[KelasController::class,'kelastambah'])->middleware('auth');
-Route::get('/kelas/hapus/{idkelas}',[KelasController::class,'kelashapus'])->middleware('auth');
-Route::put('/kelas/edit/{idkelas}', [KelasController::class,'kelasedit'])->middleware('auth');
+// Route::get('/kelas', [KelasController::class,'kelas'])->middleware('auth');
+// Route::post('/kelas/tambah',[KelasController::class,'kelastambah'])->middleware('auth');
+// Route::get('/kelas/hapus/{idkelas}',[KelasController::class,'kelashapus'])->middleware('auth');
+// Route::put('/kelas/edit/{idkelas}', [KelasController::class,'kelasedit'])->middleware('auth');
 //========================AKHIR ROUTE KELAS========================
 
 
-//=========================AWAL ROUTE RUANGAN=========================
-Route::get('/ruangan', [RuanganController::class,'ruangan'])->middleware('auth');
-Route::post('/ruangan/tambah',[RuanganController::class,'ruangantambah'])->middleware('auth');
-Route::get('/ruangan/hapus/{idruang}',[RuanganController::class,'ruanganhapus'])->middleware('auth');
-Route::put('/ruangan/edit/{idruang}', [RuanganController::class,'ruanganedit'])->middleware('auth');
-//========================AKHIR ROUTE RUANGAN========================
-
-
-
-//=========================AWAL ROUTE KELAS DETAIL=========================
-Route::get('/kelasdetail', [KelasDetailController::class,'kelasdetail'])->middleware('auth');
-Route::post('/kelasdetail/tambah',[KelasDetailController::class,'kelasdetailtambah'])->middleware('auth');
-Route::get('/kelasdetail/hapus/{idkelasdetail}',[KelasDetailController::class,'kelasdetailhapus'])->middleware('auth');
-Route::put('/kelasdetail/edit/{idkelasdetail}', [KelasDetailController::class,'kelasdetailedit'])->middleware('auth');
-//========================AKHIR ROUTE KELAS DETAIL========================
-
-
-
-
 //=========================AWAL ROUTE TAHUN AJARAN=========================
-Route::get('/thnajaran', [TahunAjaranController::class,'thnajaran'])->middleware('auth');
-Route::post('/thnajaran/tambah',[TahunAjaranController::class,'thnajarantambah'])->middleware('auth');
-Route::get('/thnajaran/hapus/{idthnajaran}',[TahunAjaranController::class,'thnajaranhapus'])->middleware('auth');
-Route::put('/thnajaran/edit/{idthnajaran}', [TahunAjaranController::class,'thnajaranedit'])->middleware('auth');
+// Route::get('/thnajaran', [TahunAjaranController::class,'thnajaran'])->middleware('auth');
+// Route::post('/thnajaran/tambah',[TahunAjaranController::class,'thnajarantambah'])->middleware('auth');
+// Route::get('/thnajaran/hapus/{idthnajaran}',[TahunAjaranController::class,'thnajaranhapus'])->middleware('auth');
+// Route::put('/thnajaran/edit/{idthnajaran}', [TahunAjaranController::class,'thnajaranedit'])->middleware('auth');
 //========================AKHIR ROUTE TAHUN AJARAN========================
 
 
@@ -109,8 +91,8 @@ Route::put('/thnajaran/edit/{idthnajaran}', [TahunAjaranController::class,'thnaj
     //AWAL CRUD SISWA
 
     //AWAL CARI SISWA
-    Route::get('/siswadetail', [SiswaController::class,'siswadetail'])->middleware('auth');
-    Route::get('/siswadetail/cari', [SiswaController::class,'siswadetailcari'])->middleware('auth');
+    // Route::get('/siswadetail', [SiswaController::class,'siswadetail'])->middleware('auth');
+    // Route::get('/siswadetail/cari', [SiswaController::class,'siswadetailcari'])->middleware('auth');
     //AKHIR CARI SISWA
 
 //========================AKHIR ROUTE SISWA========================
@@ -134,63 +116,6 @@ Route::put('/thnajaran/edit/{idthnajaran}', [TahunAjaranController::class,'thnaj
 //========================AKHIR ROUTE ORANG TUA========================
 
 
-//========================AWAL ROUTE MUTASI KELAS SISWA========================
-Route::get('/mutasikelas',[MutasiKelasController::class,'mutasikelas'])->middleware('auth');
-Route::get('/mutasikelas/cari',[MutasiKelasController::class,'mutasikelascari'])->middleware('auth');
-Route::post('/mutasikelas/proses',[MutasiKelasController::class,'mutasikelasproses'])->middleware('auth');
-//========================AKHIR ROUTE MUTASI KELAS SISWA========================
-
-
-
-//=========================AWAL ROUTE SISWA KELAS=========================
-Route::get('/siswakelas', [SiswaKelasController::class,'siswakelas'])->middleware('auth');
-Route::get('/siswakelas/cari',[SiswaKelasController::class,'siswakelascari'])->middleware('auth');
-
-
-Route::post('/siswakelas/tambah',[SiswaKelasController::class,'siswakelastambah'])->middleware('auth');
-Route::get('/siswakelas/hapus/{idsiswakelas}',[SiswaKelasController::class,'siswakelashapus'])->middleware('auth');
-Route::put('/siswakelas/edit/{idsiswakelas}', [SiswaKelasController::class,'siswakelasedit'])->middleware('auth');
-
-// Route::get('/siswakelas', function ()
-// {
-//         $kelas = Carbon::parse(request()->kelas);
-//         $data = App\Models\KelasModel::where('kelas',[$kelas])->get();
-
-//     return view('siswakelas', compact('data'))
-// })->middleware('auth');
-
-//========================AKHIR ROUTE SISWA KELAS========================
-
-
-//=========================AWAL ROUTE BAYAR SPP=========================
-Route::get('/spp', [SppController::class,'spp'])->middleware('auth');
-Route::get('/spp/cari',[SppController::class,'sppcari'])->middleware('auth');
-Route::post('/spp/tambah',[SppController::class,'spptambah'])->middleware('auth');
-//========================AKHIR ROUTE BAYAR SPP========================
-
-//=========================AWAL ROUTE BAYAR DSP=========================
-Route::get('/dsp',[DspController::class,'dsp'])->middleware('auth');
-Route::get('/dsp/cari',[DspController::class,'dspcari'])->middleware('auth');
-Route::post('/dsp/tambah',[DspController::class,'dsptambah'])->middleware('auth');
-//========================AKHIR ROUTE BAYAR DSP========================
-
-
-
-//=========================AWAL ROUTE LAPORAN PEMBAYARAN=========================
-Route::get('/laporan',[LaporanController::class.'laporan'])->middleware('auth');
-Route::get('/laporan/cari',[LaporanController::class.'laporancari'])->middleware('auth');
-Route::get('/laporan/cetakpdf',[LaporanController::class.'laporancetakpdf'])->middleware('auth');
-//========================AKHIR ROUTE LAPORAN PEMBAYARAN========================
-
-
-//=========================AWAL ROUTE JENIS BAYAR DETAIL=========================
-Route::get('/jenisbayardetail',[JenisBayarDetailController::class,'tampil'])->middleware('auth');
-Route::post('/jenisbayardetail/tambah',[JenisBayarDetailController::class,'tambah'])->middleware('auth');
-Route::get('/jenisbayardetail/hapus/{idjenisbayardetail}',[JenisBayarDetailController::class,'hapus'])->middleware('auth');
-Route::put('/jenisbayardetail/edit/{idjenisbayardetail}',[JenisBayarDetailController::class,'edit'])->middleware('auth');
-//========================AKHIR ROUTE JENIS BAYAR DETAIL========================
-
-
 
 //=========================AWAL ROUTE PEGAWAI =========================
 Route::get('/pegawai',[PegawaiController::class,'index'])->middleware('auth')->name('pegawai');
@@ -204,14 +129,14 @@ Route::delete('/pegawai/{id}', [PegawaiController::class, 'destroy'])->middlewar
 
 
 
-//=========================AWAL ROUTE PEGAWAI =========================
+//=========================AWAL ROUTE JABATAN =========================
 Route::get('/jabatan',[JabatanController::class,'index'])->middleware('auth')->name('jabatan');
 
 Route::post('/jabatan/store',[JabatanController::class,'store'])->middleware('auth')->name('jabatan.store');
 Route::put('/jabatan/{id}',[JabatanController::class,'update'])->middleware('auth')->name('jabatan.update');
 Route::delete('/jabatan/{id}',[JabatanController::class,'destroy'])->middleware('auth')->name('jabatan.destroy');
 
-//========================AKHIR ROUTE PEGAWAI ========================
+//========================AKHIR ROUTE JABATAN ========================
 
 
 
@@ -227,9 +152,6 @@ Route::get('/bukutamu/input/umum',[BukuTamuController::class,'input_umum'])->mid
 
 Route::post('/bukutamu/store',[BukuTamuController::class,'store'])->middleware('auth')->name('bukutamu.store');
 
-Route::get('/getPegawai/{id}', [BukuTamuController::class, 'getPegawai']);
-Route::get('/getOrangtua/{idsiswa}', [BukuTamuController::class, 'getOrangtua']);
-
 //========================AKHIR ROUTE BUKU TAMU ========================
 
 
@@ -244,3 +166,17 @@ Route::get('/orangtua/input',[OrangtuaController::class,'create'])->middleware('
 Route::post('/orangtua/store',[OrangtuaController::class,'store'])->middleware('auth')->name('orangtua.store');
 
 //========================AKHIR ROUTE ORANG TUA ========================
+
+
+
+//========================AKHIR ROUTE API REQUEST DATA ========================
+
+Route::get('/getPegawai/{id}', [BukuTamuController::class, 'getPegawai']);
+Route::get('/getOrangtua/{idsiswa}', [BukuTamuController::class, 'getOrangtua']);
+
+// Route::get('/admin/grafik', [BukuTamuController::class, 'grafik'])->name('admin.grafik');
+Route::get('/admin/grafik-data', [BukuTamuController::class, 'grafikData'])->name('admin.grafik.data');
+
+//========================AKHIR ROUTE API REQUEST DATA ========================
+
+
