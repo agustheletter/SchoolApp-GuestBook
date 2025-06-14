332f59c4d3c7e29945505111a404a28f06410d9c
Adre <dreenation21@gmail.com>
2025-06-14 13:03:28 +0700

    feat: update old value login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -54,7 +54,7 @@
         @csrf
         <div class="mb-4">
             <label for="email" class="block text-white font-semibold mb-1">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" required autofocus>
+            <input type="email" name="email" id="email" placeholder="Email" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" value="{{ old('email') }}" required autofocus>
         </div>
         <div class="mb-4">
             <label for="password" class="block text-white font-semibold mb-1">Password</label>
