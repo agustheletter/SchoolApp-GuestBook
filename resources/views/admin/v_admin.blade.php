5ae0913d3c9c73985e443824497346d8ca36c5aa
Adre <dreenation21@gmail.com>
2025-06-23 07:46:33 +0700

    feat: update admin page

--- resources/views/admin/v_admin.blade.php
+++ resources/views/admin/v_admin.blade.php
@@ -13,27 +13,75 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Font Awesome (diperlukan jika masih menggunakan ikon-ikon dari Font Awesome) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
     <!-- DataTables CSS -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css"> --}}
+
+    {{-- OLD --}}
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('TemplateAdminLTE') }}/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('TemplateAdminLTE') }}/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+
+
+    <!--DATA TABLE MENGGUNKAN CDN-->
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css"> --}}
+
+
+
+    <!--AWAL LIBRARY SELECT 2-->
+    <script src="{{ asset('TemplateAdminLTE') }}/plugins/jquery/jquery.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('TemplateAdminLTE') }}/dist/select2/css/select2.min.css" />
+    <script src="{{ asset('TemplateAdminLTE') }}/dist/select2/js/select2.full.min.js"></script>
+    <!--AKHIR LIBRARY SELECT 2-->
+
+
+    <!--AWAL LIBRARY DATA TABLE-->
+    <script type="text/javascript" src="{{ asset('TemplateAdminLTE') }}/dist/datatable/DataTables-1.11.5/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" type="text/css" href="{{ asset('TemplateAdminLTE') }}/dist/datatable/DataTables-1.11.5/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('TemplateAdminLTE') }}/dist/datatable/DataTables-1.11.5/css/dataTables.bootstrap.css">
+    <!--AKHIR LIBRARY DATA TABLE-->
     @stack('styles')
 </head>
 
 <body class="bg-gray-50 font-sans">
     <div class="min-h-screen flex flex-col">
         @include('admin.v_header')
-        
+
         <div class="flex flex-1">
             @include('admin.v_sidebar')
-            
+
             <main class="flex-1 p-6 bg-gray-50">
                 @yield('konten')
             </main>
         </div>
-        
+
         @include('admin.v_footer')
     </div>
 
     <!-- Scripts -->
+
+        {{-- OLD --}}
+    <!-- jQuery -->
+    <!--<script src="{{ asset('TemplateAdminLTE') }}/plugins/jquery/jquery.min.js"></script>-->
+
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('TemplateAdminLTE') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!--<script src="{{ asset('TemplateAdminLTE') }}/dist/select2/js/select2.full.min.js"></script>-->
+
+    <!-- AdminLTE App -->
+    <script src="{{ asset('TemplateAdminLTE') }}/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('TemplateAdminLTE') }}/dist/js/demo.js"></script>
+
+
+        {{-- NEW --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <script>
