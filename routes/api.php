7a98b1be4b8150c3d36456a399ad572851921764
Adre <dreenation21@gmail.com>
2025-10-07 06:56:44 +0700

    feat: update orangtua api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\SiswaController;
+use App\Http\Controllers\API\OrangtuaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,10 @@ Route::post('/siswa', [SiswaController::class, 'store']);
 // NOTE: Untuk update, kita pakai POST karena form-data (untuk upload file) tidak sepenuhnya support method PUT/PATCH
 Route::post('/siswa/{id}', [SiswaController::class, 'update']);
 Route::delete('/siswa/{id}', [SiswaController::class, 'destroy']);
+
+// Route untuk resource Orang Tua
+Route::get('/orangtua', [OrangtuaController::class, 'index']);
+Route::get('/orangtua/{id}', [OrangtuaController::class, 'show']);
+Route::post('/orangtua', [OrangtuaController::class, 'store']);
+Route::put('/orangtua/{id}', [OrangtuaController::class, 'update']); // Menggunakan PUT untuk update
+Route::delete('/orangtua/{id}', [OrangtuaController::class, 'destroy']);
