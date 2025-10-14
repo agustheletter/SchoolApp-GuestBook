461500ebc141ec7d5f48213e34e2d2af8f8d57de
Adre <dreenation21@gmail.com>
2025-10-14 11:32:45 +0700

    feat: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,25 +32,25 @@ class DatabaseSeeder extends Seeder
         $this->call(SemesterSeeder::class);
 
         $this->call(JabatanSeeder::class);
-        DB::table('tbl_jabatan')->insert([
-            ['nama_jabatan' => 'Developer'],
-        ]);
 
-        $this->call(PegawaiSeeder::class);
-        DB::table('tbl_pegawai')->insert([
-            ['nama_pegawai' => 'Adrenalin', 'jenis_kelamin' => 'Laki-laki', 'id_jabatan' => 7, 'id_agama' => 1, 'kontak' => '088222134661'],
-            ['nama_pegawai' => 'Evliya', 'jenis_kelamin' => 'Perempuan', 'id_jabatan' => 7, 'id_agama' => 1, 'kontak' => '081222678810'],
-        ]);
+        // DB::table('tbl_jabatan')->insert([
+        //     ['nama_jabatan' => 'Developer'],
+        // ]);
+
+        // $this->call(PegawaiSeeder::class);
+        // DB::table('tbl_pegawai')->insert([
+        //     ['nama_pegawai' => 'Adrenalin', 'jenis_kelamin' => 'Laki-laki', 'id_jabatan' => 7, 'id_agama' => 1, 'kontak' => '088222134661'],
+        //     ['nama_pegawai' => 'Evliya', 'jenis_kelamin' => 'Perempuan', 'id_jabatan' => 7, 'id_agama' => 1, 'kontak' => '081222678810'],
+        // ]);
 
         // $this->call(SiswaSeeder::class);
 
-        DB::table('tbl_siswa')->insert([
-        // KELAS 10
-            ['nis' => '251118435', 'nisn' => '0098224676', 'namasiswa' => 'Adrenalin Muhammad Dewangga', 'tempatlahir' => '0', 'tgllahir' => '2000-01-01', 'jk' => 'L', 'alamat' => '0', 'idagama' => '1',  'kontak' => '0', 'photosiswa' => '0', 'idthnmasuk' => 4],
-            ['nis' => '251118435', 'nisn' => '0098224676', 'namasiswa' => 'Evliya Satari Nurarifah', 'tempatlahir' => '0', 'tgllahir' => '2000-01-01', 'jk' => 'P', 'alamat' => '0', 'idagama' => '1',  'kontak' => '0', 'photosiswa' => '0', 'idthnmasuk' => 4],
-        ]);
+        // DB::table('tbl_siswa')->insert([
+        //     ['nis' => '251118435', 'nisn' => '0098224676', 'namasiswa' => 'Adrenalin Muhammad Dewangga', 'tempatlahir' => '0', 'tgllahir' => '2000-01-01', 'jk' => 'L', 'alamat' => '0', 'idagama' => '1',  'kontak' => '0', 'photosiswa' => '0', 'idthnmasuk' => 4],
+        //     ['nis' => '251118435', 'nisn' => '0098224676', 'namasiswa' => 'Evliya Satari Nurarifah', 'tempatlahir' => '0', 'tgllahir' => '2000-01-01', 'jk' => 'P', 'alamat' => '0', 'idagama' => '1',  'kontak' => '0', 'photosiswa' => '0', 'idthnmasuk' => 4],
+        // ]);
 
-        $this->call(OrangtuaSeeder::class);
-        $this->call(BukuTamuSeeder::class);
+        // $this->call(OrangtuaSeeder::class);
+        // $this->call(BukuTamuSeeder::class);
     }
 }
