8e68a2c2a72ab4a55eb50b1989649a9ecaffe97b
Adre <dreenation21@gmail.com>
2025-10-14 13:29:57 +0700

    feat: update table bukutamu

--- database/migrations/2025_03_06_104025_create_bukutamu_table.php
+++ database/migrations/2025_03_06_104025_create_bukutamu_table.php
@@ -10,11 +10,7 @@ return new class extends Migration {
         Schema::create('tbl_bukutamu', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
             $table->enum('role', ['ortu', 'umum']);
-
-            $table->unsignedBigInteger('idagama');
-            $table->foreign('idagama')->references('idagama')->on('tbl_agama')->onDelete('cascade');
 
             $table->unsignedBigInteger('idsiswa')->nullable();
             $table->foreign('idsiswa')->references('idsiswa')->on('tbl_siswa')->onDelete('cascade');
@@ -25,6 +21,7 @@ return new class extends Migration {
             $table->foreignId('id_jabatan')->constrained('tbl_jabatan');
             $table->foreignId('id_pegawai')->constrained('tbl_pegawai');
             $table->text('keperluan');
+            $table->longText('foto_tamu')->nullable();
             // $table->softDeletes();
             $table->timestamps();
         });
