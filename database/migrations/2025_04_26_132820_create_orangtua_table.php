2018eb04384370067caec7b0178e6ef9a3f2e772
Adre <dreenation21@gmail.com>
2025-10-14 11:21:55 +0700

    feat: update table ortu

--- database/migrations/2025_04_26_132820_create_orangtua_table.php
+++ database/migrations/2025_04_26_132820_create_orangtua_table.php
@@ -11,14 +11,44 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tbl_orangtua', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_ortu');
-            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
-            $table->unsignedBigInteger('idsiswa')->nullable();
-            $table->foreign('idsiswa')->references('idsiswa')->on('tbl_siswa')->onDelete('cascade');
-            $table->string('kontak')->nullable();
+        Schema::create('tbl_ortu', function (Blueprint $table) {
+            $table->increments('idortu');
+            $table->integer('idsiswa');
+
+            //ayah
+            $table->string('nama_ayah');
+            $table->date('tgllahir_ayah')->nullable();
+            $table->string('pendidikan_ayah');
+            $table->string('pekerjaan_ayah');
+            $table->string('penghasilan_ayah');
+            $table->string('nik_ayah');
+            $table->string('hp_ayah');
+            $table->string('alamat_ayah');
+
+
+
+            //ibu
+            $table->string('nama_ibu');
+            $table->date('tgllahir_ibu')->nullable();
+            $table->string('pendidikan_ibu');
+            $table->string('pekerjaan_ibu');
+            $table->string('penghasilan_ibu');
+            $table->string('nik_ibu');
+            $table->string('hp_ibu');
+            $table->string('alamat_ibu');
+
+            //wali
+            $table->string('nama_wali')->nullable();
+            $table->date('tgllahir_wali')->nullable();
+            $table->string('pendidikan_wali')->nullable();
+            $table->string('pekerjaan_wali')->nullable();
+            $table->string('penghasilan_wali')->nullable();
+            $table->string('nik_wali')->nullable();
+            $table->string('hp_wali')->nullable();
+            $table->string('alamat_wali')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +57,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tbl_orangtua');
+        // Schema::dropIfExists('tbl_ortu');
     }
 };
